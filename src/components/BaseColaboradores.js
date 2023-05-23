afcb7251482ade1fe2169e12c101bf146b3df4c1
Alex Araya <alex.araya.r@gmail.com>
2023-05-22 21:53:12 -0400

    primer commit

---
+++ src/components/BaseColaboradores.js
@@ -0,0 +1,18 @@
+  const BaseColaboradores = [
+    {
+    id: "1",
+    nombre: "Alex Araya",
+    correo: "alex@mailcolaboradores.com"
+    },
+    {
+    id: "2",
+    nombre: "Pablo Marmol",
+    correo: "pablo@mailcolaboradores.com"
+    },
+    {
+    id: "3",
+    nombre: "Super Sonico",
+    correo: "super@mailcolaboradores.com"
+    },
+    ]
+    export default BaseColaboradores;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return <>
+      <div class="classFooter">
+        <h6>Hecho con todo el Fuaa!! 💪🏼 por Alex</h6>
+      </div>
+    </>
+  }
+  
+  export default Footer;
